e6fca895ea6d30f9217165b82ed05c7ac66317d7
marcocaruso01 <marco.caruso01@gmail.com>
2026-07-28 01:01:07 +0200

    Update V39.9.1 Itadori rule test

--- tests/v399-itadori-ui.test.js
+++ tests/v399-itadori-ui.test.js
@@ -2,7 +2,7 @@
 const assert=require('node:assert/strict');
 const rules=require('../v399-itadori-ui-progression.js');
 
-assert.equal(rules.version,'39.9.0');
+assert.equal(rules.version,'39.9.1');
 assert.equal(rules.maxLifeForFingers(0),4);
 assert.equal(rules.maxLifeForFingers(4),4);
 assert.equal(rules.maxLifeForFingers(8),5);
@@ -15,4 +15,4 @@ assert.match(rules.thresholdUpgrade(12),/\+2 EM massima/);
 assert.match(rules.thresholdUpgrade(16),/\+1 Vita massima/);
 assert.match(rules.thresholdUpgrade(20),/\+1 Corpo permanente/);
 assert.deepEqual(rules.fingerBonuses(20),{body:5,life:4,energy:6});
-console.log('V39.9 Itadori UI/progression tests passed');
+console.log('V39.9.1 Itadori UI/progression tests passed');
